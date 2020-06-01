3369a4a034d4c8653a22d3ccff79d487d59e0410
alber <alber@DESKTOP-4DLBMOL>
2020-06-01 08:52:48 +0200

    Version 1.0.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,12 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
+ * Version 1.0
+ * 
  */
 
 /**
  *
- * @author alber
+ * @author Alberto
  */
 public class Calculadora {
     
